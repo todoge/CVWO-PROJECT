2d97727e158cad9e87ec9c2ca1aa21bb3fb09499
Peh Jun Siang <austin9090@hotmail.com>
2020-01-10 20:20:08 +0800

    added User-todos asscociation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length:{maximum:50}
   validates :description, presence: true, length:{maximum:200}
+  validates :user_id, presence:true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_24_173132) do
+ActiveRecord::Schema.define(version: 2020_01_10_095110) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
